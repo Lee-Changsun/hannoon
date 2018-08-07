ce4fc67b06cbb670b1e942f5b1934370389e2bf9
CS_NOTE_2\ckdtj <window_for_sun@naver.com>
2018-08-08 02:41:58 +0900

    db관련

---
+++ src/test/java/com/jungsun/hannoon/MySqlConnectionTest.java
@@ -0,0 +1,15 @@
+package com.jungsun.hannoon;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MySqlConnectionTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
